ff071e23b943827de568c21c65a1178fe9f5ed1c
W10 <W10@PJA>
2020-05-20 21:12:25 +0900

    Ex22

---
+++ JavaPrjEx/src/Ex22.java
@@ -0,0 +1,19 @@
+
+public class Ex22 {
+
+	public static void main(String[] args) {
+
+		
+		int x =4, y = 2;
+		int tmp;
+		
+		tmp = x;
+		x = y;
+		y =tmp;
+		
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+
+	}
+
+}
