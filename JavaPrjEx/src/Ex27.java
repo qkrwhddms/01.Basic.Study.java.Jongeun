d9eedfb5af153ad4a219beae2cfb40267d294139
W10 <W10@PJA>
2020-05-23 15:52:55 +0900

    Break 문 Ex

--- JavaPrjEx/src/Ex27.java
+++ JavaPrjEx/src/Ex27.java
@@ -2,17 +2,7 @@
 public class Ex27 {
 
 	public static void main(String[] args) {
-			      
-			      int i;
-			      
-			      for (i =1; i<=100; i++) {
-			         System.out.printf("for 문을 %d 회 실행했습니다.\n",i);
-			         if(i==57)
-			         break;
-			      }
-			      System.out.printf("for 문을 종료했습니다.\n");
-
-			   	
+		// TODO Auto-generated method stub
 
 	}
 
