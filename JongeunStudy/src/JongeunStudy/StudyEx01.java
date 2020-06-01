792563412889a4d75d5c5cf9eca13147a0d06128
W10 <qkrwhddms01@gmail.com>
2020-06-01 17:27:20 +0900

    Study Ex01

---
+++ JongeunStudy/src/JongeunStudy/StudyEx01.java
@@ -0,0 +1,12 @@
+package JongeunStudy;
+
+public class StudyEx01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello? Jongeun");
+		              // 화면에 내용을 출력
+
+	}
+
+}
