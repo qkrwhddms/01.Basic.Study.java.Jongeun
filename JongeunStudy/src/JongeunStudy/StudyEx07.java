0160b3347ad7cefeae6d8e9750d9f8ce1b1dd3ee
W10 <qkrwhddms01@gmail.com>
2020-06-09 06:36:10 +0900

    Study_Ex07

---
+++ JongeunStudy/src/JongeunStudy/StudyEx07.java
@@ -0,0 +1,14 @@
+package JongeunStudy;
+
+public class StudyEx07 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("100 + 100");  // 모두 글자로 취급한다.
+		System.out.printf("\n");  // System.out.printf()는 행이 넘어가지 않으므로 강제로 행이 넘어가게 한다.
+		System.out.printf("%d", 100 + 100);  // 숫자를 계싼해서 결과를 출력한다.
+		System.out.printf("\n");  // System.out.printf()는 행이 넘어가지 않으므로 강제로 행이 넘어가게한다.
+
+	}
+
+}
