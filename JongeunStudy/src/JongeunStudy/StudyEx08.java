7f16e9ef5a3ae73d853d71086a0592f63aa40251
W10 <qkrwhddms01@gmail.com>
2020-06-10 05:55:51 +0900

    Study_Ex08

---
+++ JongeunStudy/src/JongeunStudy/StudyEx08.java
@@ -0,0 +1,14 @@
+package JongeunStudy;
+
+public class StudyEx08 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("%d %d", 100, 200);
+		System.out.printf("\n");
+		System.out.printf("%d", 100);
+		System.out.printf("\n");
+
+	}
+
+}
