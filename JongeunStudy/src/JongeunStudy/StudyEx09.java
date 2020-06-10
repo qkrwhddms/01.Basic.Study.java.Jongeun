673deeb9ca6adbddf05308167d2407fda137aa32
W10 <qkrwhddms01@gmail.com>
2020-06-11 06:58:39 +0900

    Study_Ex09

---
+++ JongeunStudy/src/JongeunStudy/StudyEx09.java
@@ -0,0 +1,13 @@
+package JongeunStudy;
+
+public class StudyEx09 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.printf("%d / %d = %f", 100, 200, 0.5);  // %f는 실수 (소수점이 있는 수)
+		System.out.printf("\n");
+
+	}
+
+}
