3de0db64d585c0d181327d79708fd1bbb3b36f63
W10 <qkrwhddms01@gmail.com>
2020-06-12 06:44:23 +0900

    Study_Ex10

---
+++ JongeunStudy/src/JongeunStudy/StudyEx10.java
@@ -0,0 +1,13 @@
+package JongeunStudy;
+
+public class StudyEx10 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("%d / %d = %f \n", 100, 200, 0.5);  //정수 2개와 실수 1개를 출력
+		System.out.printf("%c %c \n", 'a', 'k');  //문자 2개를 출력
+		System.out.printf("%s %s \n", "IT CookBook", "JAVA");  //문자열 2개를 출력
+
+	}
+
+}
