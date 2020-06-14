a9bee4e16f244028e75cf9088136326dc20e34d3
W10 <qkrwhddms01@gmail.com>
2020-06-15 06:49:04 +0900

    Study_Ex11

---
+++ JongeunStudy/src/JongeunStudy/StudyEx11.java
@@ -0,0 +1,19 @@
+package JongeunStudy;
+
+public class StudyEx11 {
+
+	public static void main(String[] args) {
+		System.out.printf("%d\n", 123);  //
+		System.out.printf("%5d\n", 123);  // 정수형 서식을 활용했다
+		System.out.printf("%05d\n", 123);  //
+		
+		System.out.printf("%f\n", 123.45);  //
+		System.out.printf("%7.1f\n", 123.45);  // 실수형 서식을 활용햇다.
+		System.out.printf("%7.3f\n", 123.45);  //
+		
+		System.out.printf("%s\n", "Hi~Java");  // 문자열형 서식을 활용했다.
+		System.out.printf("%10s\n", "Hi~Java");  //
+
+	}
+
+}
