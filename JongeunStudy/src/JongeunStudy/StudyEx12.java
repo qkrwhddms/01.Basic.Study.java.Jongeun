aa571e555be60c73306ea325f696f3cfeaad9ab2
W10 <qkrwhddms01@gmail.com>
2020-06-16 06:55:07 +0900

    Study_Ex12

---
+++ JongeunStudy/src/JongeunStudy/StudyEx12.java
@@ -0,0 +1,14 @@
+package JongeunStudy;
+
+public class StudyEx12 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("\n줄바꿈\n연습 \n");  //
+		System.out.printf("\t탭키\t \n");  //
+		System.out.printf("이것을\r덮습니다 \n");  //   다양한 서식 문자를 활용했다.
+		System.out.printf("글자가 \"강조\"되는 효과 \n");  //
+		System.out.printf("\\\\\\ 역슬래시 세개 출력 \n");  //
+	}
+
+}
