c7cc28bbede5e77c7ffa84cbae89b1c8b3063ad2
W10 <qkrwhddms01@gmail.com>
2020-06-18 06:53:46 +0900

    Study_Ex14

---
+++ JongeunStudy/src/JongeunStudy/StudyEx14.java
@@ -0,0 +1,21 @@
+package JongeunStudy;
+
+public class StudyEx14 {
+
+	public static void main(String[] args) {
+		
+		int a, b;   // 정수형 변수가 2개
+		float c, d;   // 실수형 변수가 2개
+		
+		a = 100;   // a에 정수 100을 대입
+		b = a;   // b에 a값을 대입
+		
+		c = 111.1f;   // c에 실수 111.1을 대입
+		d = c;   // d에 c값을 대입
+		
+		System.out.printf("a, b의 값 ==> %d, %d \n", a, b);
+		System.out.printf("c, d의 값 ==> %5.1f, %5.1f \n", c, d);
+
+	}
+
+}
