557a46d4db0e05bf8533ffc40d735602030c8609
W10 <qkrwhddms01@gmail.com>
2020-06-22 06:50:23 +0900

    Study_Ex16

---
+++ JongeunStudy/src/JongeunStudy/StudyEx16.java
@@ -0,0 +1,16 @@
+package JongeunStudy;
+
+public class StudyEx16 {
+
+	public static void main(String[] args) {
+		
+		int a = 100, b = 200;   // 정수형 변수 a와 b에 값을 지정
+		float result;   // 실수형 변수 result를 선언
+		
+		result = a / b;   // a를 b로 나눈 다음 결과를 실수형 변수 result에 대입(0.5를예상)
+		
+		System.out.printf("%f \n", result);
+
+	}
+
+}
