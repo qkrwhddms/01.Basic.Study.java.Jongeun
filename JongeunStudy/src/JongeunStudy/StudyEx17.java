c46fe46823d5de8109a1785b9f6a21867188c56b
W10 <qkrwhddms01@gmail.com>
2020-06-23 06:44:46 +0900

    Study_Ex17

---
+++ JongeunStudy/src/JongeunStudy/StudyEx17.java
@@ -0,0 +1,15 @@
+package JongeunStudy;
+
+public class StudyEx17 {
+
+	public static void main(String[] args) {
+		
+		float a = 0.1234567890123456789012345f;   // float형 변수 a에 정밀도 25자리를 입력
+		double b = 0.1234567890123456789012345;   // double형 변수 b에 정밀도 25자리를 입력
+		
+		System.out.printf("%30.25f \n", a);
+		System.out.printf("%30.25f \n", b);
+
+	}
+
+}
