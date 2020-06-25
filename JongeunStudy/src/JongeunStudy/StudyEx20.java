2e1cffe317813951fb80c682130c1c996e147608
W10 <qkrwhddms01@gmail.com>
2020-06-26 06:54:56 +0900

    Study_Ex20

---
+++ JongeunStudy/src/JongeunStudy/StudyEx20.java
@@ -0,0 +1,17 @@
+package JongeunStudy;
+
+public class StudyEx20 {
+
+	public static void main(String[] args) {
+		
+		boolean boo1, boo2;   // 불형 변수 2개를 선언
+		
+		boo1 = true;   // boo1 변수에 true를 대입
+		System.out.printf("%s \n", boo1);   // boo1 변수를 출력
+		
+		boo2 = (10 == 20);   // boo2 변수에 10과 20을 비교한 결과를 대입
+		System.out.printf("%s \n", boo2);   // boo2 변수를 출력
+
+	}
+
+}
