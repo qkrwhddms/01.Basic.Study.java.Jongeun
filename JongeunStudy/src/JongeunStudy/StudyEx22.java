a0f21e36e9ff12e6f31caac56b83640b5b5658f8
W10 <qkrwhddms01@gmail.com>
2020-06-30 00:04:52 +0900

    Study_Ex22

---
+++ JongeunStudy/src/JongeunStudy/StudyEx22.java
@@ -0,0 +1,27 @@
+package JongeunStudy;
+
+public class StudyEx22 {
+
+	public static void main(String[] args) {
+		
+		int a, b = 5, c = 3;
+		
+		a = b + c;   // b와 c를 더하기 연산하여 a에 대입한다.
+		System.out.printf("%d + %d = %d \n", b, c, a);
+		
+		a = b - c;   // b와 c를 빼기 연산하여 a에 대입한다.
+		System.out.printf("%d - %d = %d \n", b, c, a);
+		
+		a = b * c;   // b와 c를 곱하기 연산하여 a에 대입한다.
+		System.out.printf("%d * %d = %d \n", b, c, a);
+		
+		a = b / c;   // b와 c를 나누기 연산하여 a에 대입한다.
+		System.out.printf("%d / %d = %d \n", b, c, a);
+		
+		a = b % c;   // b와 c를 나누기 연산하여 나머지 값을 a에 대입한다.
+		System.out.printf("%d %% %d = %d \n", b, c, a);
+		
+
+	}
+
+}
