3705d73d7ff2853f89e6ba0f76dc976a849fc681
W10 <qkrwhddms01@gmail.com>
2020-06-29 14:44:32 +0900

    Study_Ex21

---
+++ JongeunStudy/src/JongeunStudy/StudyEx21.java
@@ -0,0 +1,18 @@
+package JongeunStudy;
+
+public class StudyEx21 {
+
+	public static void main(String[] args) {
+		
+		String str1 = "IT CookBook 입니다.";   // str1 문자열 변수를 선언하고, 선언과 동시에 값을 입력한다.
+		String str2 = "10";   // str2, str3 문자열 변수를 선언하고 각각에
+		String str3 = "20";   // 문자열 "10"과 "20"을 대입한다.
+		
+		str1 = "Java 입니다.";   // str1 문자열 변수에 값을 대입한다.
+		
+		System.out.printf("%s \n", str1);   // str1을 출력한다.
+		System.out.printf("%s \n", str2 + str3);   // str2와 str3의 합계를 출력한다.
+		
+	}
+
+}
