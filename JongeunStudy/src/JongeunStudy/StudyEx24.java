08b1df838a180187cbc31dd539799c94551740fa
W10 <qkrwhddms01@gmail.com>
2020-07-02 00:20:42 +0900

    Study_Ex25

---
+++ JongeunStudy/src/JongeunStudy/StudyEx24.java
@@ -0,0 +1,31 @@
+package JongeunStudy;
+
+public class StudyEx24 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		a++;   // a = a+1과 동일하다.
+		System.out.printf(" a ++ ==> %d \n", a);
+		
+		a--;   // a = a-1과 동일하다.
+		System.out.printf(" a -- == %d \n", a);
+		
+		a += 5;   // a = a+5와 동일하다.
+		System.out.printf(" a += 5 ==> %d \n", a);
+		
+		a -=5;   // a = a-5와 동일하다.
+		System.out.printf(" a -= 5 ==> %d \n", a);
+		
+		a *= 5;   // a = a*5와 동일하다.
+		System.out.printf(" a *= 5 ==> %d \n", a);
+		
+		a /= 5;   // a = a/5와 동잏하다. 
+		System.out.printf(" a /= 5 ==> %d \n", a);
+		
+		a %= 5;   // a = a%5와 동일하다.
+		System.out.printf(" a %%= 5 ==> %d \n", a);
+	}
+
+}
