53da59e75870f9bf6beef5ec9b0def30fa80b4cf
W10 <qkrwhddms01@gmail.com>
2020-07-06 11:44:03 +0900

    Study_Ex26

---
+++ JongeunStudy/src/JongeunStudy/StudyEx26.java
@@ -0,0 +1,20 @@
+package JongeunStudy;
+
+public class StudyEx26 {
+
+	public static void main(String[] args) {
+		
+		int a = 100, b = 200;
+		
+		System.out.printf(" %d == %d 는 %s 이다.\n", a, b, a == b);   //
+		System.out.printf(" %d != %d 는 %s 이다.\n", a, b, a != b);   //  같다, 같지 않다.
+		System.out.printf(" %d > %d 는 %s 이다.\n", a, b, a > b);   //   크다, 작다, 크거나
+		System.out.printf(" %d < %d 는 %s 이다.\n", a, b, a < b);   //   같다, 작거나 같다의
+		System.out.printf(" %d >= %d 는 %s 이다.\n", a, b, a >= b);   //  관계 연산자를
+		System.out.printf(" %d <= %d 는 %s 이다.\n", a, b, a <= b);   //   실행한다.
+		
+		System.out.printf(" %d = %d 는 %s 이다.\n", a, b, a = b);   // 대입 연산자를 실행한다.
+
+	}
+
+}
