d9051ad9230962381ae0fd8464d0900e683c4199
W10 <qkrwhddms01@gmail.com>
2020-07-08 15:28:12 +0900

    Study_Ex28

---
+++ JongeunStudy/src/JongeunStudy/StudyEx28.java
@@ -0,0 +1,19 @@
+package JongeunStudy;
+
+public class StudyEx28 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 100, num2 = -200;
+		
+		boolean a = (num1 != 0);
+		
+		boolean b = (num2 != 0);
+		
+		System.out.printf(" 상수의 AND 연산 : %s \n", a && b);
+		System.out.printf(" 상수의 OR 연산 : %s \n", a || b);
+		System.out.printf(" 상수의 NOT 연산 : %s \n", !a);
+
+	}
+
+}
