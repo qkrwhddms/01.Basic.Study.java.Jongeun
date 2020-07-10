e87ba4d5b62d0330b92d8680cee3d44a78500ccb
W10 <qkrwhddms01@gmail.com>
2020-07-10 10:57:05 +0900

    Study_Ex30

---
+++ JongeunStudy/src/JongeunStudy/StudyEx30.java
@@ -0,0 +1,14 @@
+package JongeunStudy;
+
+public class StudyEx30 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf(" 10 | 7 = %d \n", 10 | 7);   // 10과 7의 비트 논리합을 수행한다.
+		System.out.printf(" 123 | 456 = %d \n", 123 | 456);   // 123과 456의 비트 논리합을 수행한다.
+		
+		System.out.printf(" 0xFFFF | 0000 = %d \n", 0xFFFF | 0000);   // 16진수 FFFF와 0의 비트 논리합을 수행한다. 
+
+	}
+
+}
