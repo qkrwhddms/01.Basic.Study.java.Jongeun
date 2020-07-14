4ffe7c31b8fdb18194ab28459d76c130fd995fb7
W10 <qkrwhddms01@gmail.com>
2020-07-15 00:22:20 +0900

    Study_Ex33

---
+++ JongeunStudy/src/JongeunStudy/StudyEx33.java
@@ -0,0 +1,13 @@
+package JongeunStudy;
+
+public class StudyEx33 {
+
+	public static void main(String[] args) {
+		
+		int a = 12345;
+		
+		System.out.printf(" %d \n", ~a +1);   // a 값의 2의 보수를 구한다.
+
+	}
+
+}
