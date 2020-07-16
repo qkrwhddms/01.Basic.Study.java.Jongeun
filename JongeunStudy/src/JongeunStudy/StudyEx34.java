d38042648aebaad8bab6dca2dd2b84ba9845a2af
W10 <qkrwhddms01@gmail.com>
2020-07-16 18:48:20 +0900

    Study_Ex34

---
+++ JongeunStudy/src/JongeunStudy/StudyEx34.java
@@ -0,0 +1,14 @@
+package JongeunStudy;
+
+public class StudyEx34 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		System.out.printf("%d 를 왼쪽 1회 시프트하면 %d 이다. \n", a, a<<1);  // 왼쪽
+		System.out.printf("%d 를 왼쪽 2회 시프트하면 %d 이다. \n", a, a<<2);  // 시프트한 결과를
+		System.out.printf("%d 를 왼쪽 3회 시프트하면 %d 이다. \n", a, a<<3);  // 출력한다.
+
+	}
+
+}
