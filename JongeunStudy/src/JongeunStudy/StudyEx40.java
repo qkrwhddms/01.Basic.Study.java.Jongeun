e4a3fcff3aa48418f071a99cd7f8346d4495f04c
W10 <qkrwhddms01@gmail.com>
2020-07-24 15:01:45 +0900

    Study_Ex40

---
+++ JongeunStudy/src/JongeunStudy/StudyEx40.java
@@ -0,0 +1,14 @@
+package JongeunStudy;
+
+public class StudyEx40 {
+
+	public static void main(String[] args) {
+		
+		int a = 99;
+		
+		if(a < 100)   // a가 100보다 작으므로 참이다.
+			System.out.printf("100보다 작군요..\n");
+
+	}
+
+}
