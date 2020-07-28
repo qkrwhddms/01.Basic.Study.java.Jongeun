739ac7802d498f36b068179e66d75761f21fa3f4
W10 <qkrwhddms01@gmail.com>
2020-07-28 16:08:48 +0900

    Study_Ex42

---
+++ JongeunStudy/src/JongeunStudy/StudyEx42.java
@@ -0,0 +1,18 @@
+package JongeunStudy;
+
+public class StudyEx42 {
+
+	public static void main(String[] args) {
+		
+		int a = 200;
+		
+		if(a < 100) {
+			System.out.printf("100보다 작군요..\n");             // 5행이 참이면 중괄호로
+			System.out.printf("거짓이므로 앞의 문장은 안보이겠죠?\n");   // 묶인 부분이 모두 수행된다.
+		}
+		
+		System.out.printf("프로그램 끝! \n");
+
+	}
+
+}
