181447486314a0d837adeb318936f6eb87405fb5
W10 <qkrwhddms01@gmail.com>
2020-07-29 20:11:20 +0900

    Study_Ex43

---
+++ JongeunStudy/src/JongeunStudy/StudyEx43.java
@@ -0,0 +1,16 @@
+package JongeunStudy;
+
+public class StudyEx43 {
+
+	public static void main(String[] args) {
+		
+		int a = 200;
+		
+		if(a < 100)
+			System.out.printf("100보다 작군요..\n");  // 5행이 참이면 (a가 100보다 작으면) 실행한다.
+		else
+			System.out.printf("100보다 크군요..\n");  // 5행이 거짓이면 (a가 100보다 크거나 같으면) 실행한다.
+
+	}
+
+}
